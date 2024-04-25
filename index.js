c9a1ae4634beb2df3d5abfdf8143fb578ccaa4c5
aftermight <167148913+aftermight@users.noreply.github.com>
2024-04-25 16:50:46 +0000

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const bareServer = createBareServer("/o/")
 const PORT = process.env.PORT || 8080
 
 if (config.challenge) {
-  console.log("failed password check. Usernames are: " + Object.keys(config.users))
+  console.log("Password protection enabled. Usernames are: " + Object.keys(config.users))
   console.log("Passwords are: " + Object.values(config.users))
 
   app.use(
